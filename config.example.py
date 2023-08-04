1ec7f592b955e3899b1293470065d5f448e87ff1
1F616EMO <root@1f616emo.xyz>
2023-08-04 11:57:45 +0800

    apply change from TH3

--- config.example.py
+++ config.example.py
@@ -7,3 +7,6 @@ api_hash = '0123456789abcdef0123456789abcdef'
 # Get the folloing from @botfather
 bot_token = '12345:0123456789abcdef0123456789abcdef'
 
+rlimit = 5 # Rate limit in second
+
+owner = 777000
--- main.py
+++ main.py
@@ -16,10 +16,9 @@ except ImportError:
         print("config.example.py not found, make sure you're in the script's directory!")
     exit(1)
 
-import asyncio, random
+import asyncio, random, time, logging
 from telethon import TelegramClient, functions, types, events
 from telethon.errors import *
-import logging
 
 logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)s[%(name)s]: %(message)s")
 
@@ -31,8 +30,17 @@ def meougen():
 bot = TelegramClient('bot', config.api_id, config.api_hash).start(bot_token=config.bot_token)
 client = bot
 
+rlimit = {}
+
 @bot.on(events.NewMessage)
 async def msg(event):
+    now = time.time()
+    if event.sender_id in rlimit:
+        if now - rlimit[event.sender_id] < config.rlimit:
+            if event.is_private:
+                await event.respond("太快了喵！")
+                return
+    rlimit[event.sender_id] = now
     text = event.message.text
     if (text.startswith("/start") or text.startswith("/help")) and event.is_private:
         await event.respond("私聊發送任何信息我會喵喵～\n群組發送喵叫信息我會喵喵～\n我們不會留下喵喵記錄污染貓糧哦喵～")
@@ -41,10 +49,23 @@ async def msg(event):
 
 @bot.on(events.InlineQuery)
 async def inline(event):
+    now = time.time()
+    if event.sender_id in rlimit:
+        if now - rlimit[event.sender_id] < config.rlimit:
+            await event.answer([
+                event.builder.article("太快了喵！", text="太快了喵！"),
+            ])
+            return
+    rlimit[event.sender_id] = now
     msg = meougen()
     await event.answer([
         event.builder.article(msg, text=msg),
     ])
+
+@bot.on(events.NewMessage(pattern="/meouleave"))
+async def msg(event):
+    if event.sender.id == config.owner:
+        await bot.kick_participant(event.chat, 'me')
 
 bot.run_until_disconnected()
 
